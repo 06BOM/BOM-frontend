a94d0f4e89bd3f90a353079f0055991c16aedd2c
shseok <gus0674@ajou.ac.kr>
2022-05-11 00:31:45 +0900

    Create timer UI for Quiz

--- lib/view/components/quiz/quiz_question.dart
+++ lib/view/components/quiz/quiz_question.dart
@@ -34,8 +34,8 @@ class QuizQuestions extends ConsumerWidget {
                 width: MediaQuery.of(context).size.width,
                 height: 200.0,
                 color: Colors.black.withOpacity(0.2),
-                margin: const EdgeInsets.symmetric(
-                  vertical: 60.0,
+                margin: const EdgeInsets.only(
+                  top: 80.0,
                 ),
                 child: Column(
                   // mainAxisAlignment: MainAxisAlignment.center,
@@ -87,6 +87,50 @@ class QuizQuestions extends ConsumerWidget {
               //   endIndent: 20.0,
               // ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    // padding: EdgeInsets.all(0.0),
+                    decoration: BoxDecoration(
+                        border:
+                            Border.all(color: Colors.greenAccent, width: 2)),
+                    width: 80.0,
+                    height: 80.0,
+                    child: CustomPaint(
+                      painter: timerBorderPaint(progress: 0.6, taskCompletedColor: Colors.blue, taskNotCompletedColor: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    // padding: EdgeInsets.all(0.0),
+                    decoration: BoxDecoration(
+                        border:
+                            Border.all(color: Colors.greenAccent, width: 2)),
+                    width: 180.0,
+                    height: 180.0,
+                    child: CustomPaint(
+                      painter: CrossDrawPaint(),
+                      child: Container(),
+                    ),
+                  ),
+                  Container(
+                    // padding: EdgeInsets.all(0.0),
+                    decoration: BoxDecoration(
+                        border:
+                            Border.all(color: Colors.greenAccent, width: 2)),
+                    width: 180.0,
+                    height: 180.0,
+                    child: CustomPaint(
+                      painter: RingPaint(),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: question.answers
                     .map((e) => AnswerCard(
@@ -99,32 +143,6 @@ class QuizQuestions extends ConsumerWidget {
                             .submitAnswer(question, e)))
                     .toList(),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    // padding: EdgeInsets.all(0.0),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.greenAccent, width: 2)),
-                    width: 180.0,
-                    height: 180.0,
-                    child: CustomPaint(
-                      painter: CrossDrawPaint(),
-                      child: Container(),
-                    ),
-                  ),
-                  Container(
-                    // padding: EdgeInsets.all(0.0),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.greenAccent, width: 2)),
-                    width: 180.0,
-                    height: 180.0,
-                    child: CustomPaint(
-                      painter: RingPaint(),
-                    ),
-                  ),
-                ],
-              )
             ],
           );
         });
@@ -138,12 +156,10 @@ class CrossDrawPaint extends CustomPainter {
       ..color = Colors.red
       ..style = PaintingStyle.stroke
       ..strokeWidth = 30;
-    canvas.drawLine(
-        const Offset(40, 40),
+    canvas.drawLine(const Offset(40, 40),
         Offset(size.width - 40, size.height - 40), crossBrush);
     canvas.drawLine(
-        Offset(size.width - 40, 40),
-        Offset(40, size.height - 40), crossBrush);
+        Offset(size.width - 40, 40), Offset(40, size.height - 40), crossBrush);
   }
 
   @override
@@ -153,7 +169,6 @@ class CrossDrawPaint extends CustomPainter {
 }
 
 class RingPaint extends CustomPainter {
-
   @override
   void paint(Canvas canvas, Size size) {
     final strokeWidth = size.width / 5.0;
@@ -170,4 +185,41 @@ class RingPaint extends CustomPainter {
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class timerBorderPaint extends CustomPainter {
+  // timerBorderPaint(
+  //     this.progress, this.taskNotCompletedColor, this.taskCompletedColor);
+  timerBorderPaint(
+      {required this.progress, required this.taskNotCompletedColor, required this.taskCompletedColor});
+
+  final double progress;
+  final Color taskNotCompletedColor;
+  final Color taskCompletedColor;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final strokeWidth = size.width / 15.0;
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = (size.width - strokeWidth) / 2.7; // 2일때 꽉 찬다.
+
+    final backgroundpaint = Paint()
+      ..isAntiAlias = true
+      ..color = taskNotCompletedColor
+      ..strokeWidth = strokeWidth
+      ..style = PaintingStyle.stroke; //important set stroke style
+
+    canvas.drawCircle(center, radius, backgroundpaint);
+
+    final foregraoundPaint = Paint()
+      ..isAntiAlias = true
+      ..strokeWidth = strokeWidth
+      ..color = taskCompletedColor
+      ..style = PaintingStyle.stroke;
+    canvas.drawArc(Rect.fromCircle(center: center, radius: radius),
+        -math.pi / 2, 2 * math.pi * progress, false, foregraoundPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant timerBorderPaint oldDelegate) => oldDelegate.progress != progress;
 }
