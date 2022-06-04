d6b88bfc525e18a4594c65eb2e14385da4e20ae8
shseok <gus0674@ajou.ac.kr>
2022-06-04 17:10:40 +0900

    Change UI & Add Add BUtton on Room Screen

--- lib/view/game_view.dart
+++ lib/view/game_view.dart
@@ -11,114 +11,173 @@ class GameScreen extends ConsumerStatefulWidget {
   const GameScreen({Key? key}) : super(key: key);
 
   @override
-ConsumerState<GameScreen> createState() => _GameScreenState();
+  ConsumerState<GameScreen> createState() => _GameScreenState();
 }
 
 class _GameScreenState extends ConsumerState<GameScreen> {
-
   @override
   Widget build(BuildContext context) {
     final rooms = ref.watch(roomListProvider);
     print(rooms);
     print(ref.read(roomListProvider.notifier).searchTheRoom());
     return Scaffold(
-        appBar: BomAppBar(screenName: 'game',),
-        drawer: BomMenu(),
-        body: Column(
-          children: [
-            Container(
-                child:rooms.when(
-                    data: (data) {
-                      return gridBody(data, context);
-                    },
-                    error: (e, st) => Container(),
-                    loading: () => Container()
-              )
-            )
-          ],
-        ),
-        bottomNavigationBar: BottomNavigationBarWidget(),
-        );
+      appBar: BomAppBar(
+        screenName: 'game',
+      ),
+      drawer: BomMenu(),
+      body: Column(
+        children: [
+          Container(
+              child: rooms.when(
+                  data: (data) {
+                    return gridBody(data, context);
+                  },
+                  error: (e, st) => Container(),
+                  loading: () => Container()))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        heroTag: 'add',
+        child: const Icon(Icons.add),
+        backgroundColor: const Color(0xffA876DE),
+        onPressed: () {
+          // ref.read(categoryIdToCreate.notifier).state = 1;
+          // ref.read(repetitionTypeToCreate.notifier).state = 0;
+          // ref.read(limitedDate.notifier).state = '';
+          // ref.read(selectedWeek.notifier).state = [
+          //   0,
+          //   0,
+          //   0,
+          //   0,
+          //   0,
+          //   0,
+          //   0
+          // ];
+          // Navigator.push(
+          //     context,
+          //     MaterialPageRoute(
+          //         builder: (context) =>
+          //             AddPlan()));
+          print('click');
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBarWidget(),
+    );
   }
 }
 
 Widget gridBody(List<Room> display_list, BuildContext context) {
   return display_list.length == 0
       ? Center(
-      child: Text(
-        "결과가 없습니다.",
-      ))
+          child: Text(
+          "결과가 없습니다.",
+        ))
       : Expanded(
-    // 없었을 때, verticalviewport error 해결
-    child: GridView(
-      padding: EdgeInsets.all(20),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-      ),
-      children: display_list
-          .map(
-            (bomRoom) => GestureDetector(
-          onTap: () {
-            print('clicked');
-            // _showDialog(context);
-            // Navigator.push(
-            //   context,
-            //   MaterialPageRoute(
-            //       builder: (context) =>
-            //           CharacterDetails(character: bomCharacter)),
-            // );
-          },
-          child: Container(
-            constraints: BoxConstraints(
-              maxWidth: MediaQuery.of(context).size.width * 0.4,
+          // 없었을 때, verticalviewport error 해결
+          child: GridView(
+            padding: EdgeInsets.all(20),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
             ),
-            margin: EdgeInsets.only(top: 10.0),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30.0),
-                side: BorderSide(width: 4, color: Colors.orange),
-              ),
-              // color: Colors.white,
-              elevation: 10,
-              child: Column(
-                // mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Container(
-                    // decoration: BoxDecoration(
-                    //     borderRadius: BorderRadius.all(
-                    //         Radius.circular(30.0))),
-                    padding: EdgeInsets.symmetric(
-                        vertical: 8.0, horizontal: 10.0),
-                    child: Image.network(
-                      'https://bom-mocktest.s3.ap-northeast-2.amazonaws.com/quiz.png',
-                      width: 60,
-                      height: 60,
-                      fit: BoxFit.fitHeight,
-                    ),
-                  ),
-                  Expanded(
+            children: display_list
+                .map(
+                  (bomRoom) => GestureDetector(
+                    onTap: () {
+                      print('clicked');
+                      // _showDialog(context);
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //       builder: (context) =>
+                      //           CharacterDetails(character: bomCharacter)),
+                      // );
+                    },
                     child: Container(
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                          color: Colors.orange,
-                          borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(30.0),
-                              bottomRight: Radius.circular(30.0))),
-                      child: Text('${bomRoom.roomName}',
-                          style: TextStyle(
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white)),
+                      constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width * 0.4,
+                      ),
+                      margin: EdgeInsets.only(top: 10.0),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        ),
+                        elevation: 10,
+                        child: Column(
+                          // mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20.0),
+                                topRight: Radius.circular(20.0),
+                              ),
+                              child: Image.network(
+                                'https://bom-mocktest.s3.ap-northeast-2.amazonaws.com/quiz.jpg',
+                                fit: BoxFit.fill,
+                              ), // Text(key['title']),
+                            ),
+                            Expanded(
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 3.0, horizontal: 8.0),
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                    // color: Color(0xffA876DE),
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.only(
+                                        bottomLeft: Radius.circular(20.0),
+                                        bottomRight: Radius.circular(20.0))),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                        // margin: EdgeInsets.only(left: 7.0),
+                                        padding: EdgeInsets.all(10.0),
+                                        decoration: BoxDecoration(
+                                            color: Colors.grey[200],
+                                            borderRadius:
+                                                BorderRadius.circular(13.0)),
+                                        child: Text('${bomRoom.subject}',
+                                            style: TextStyle(
+                                                fontSize: 15.0,
+                                                fontWeight: FontWeight.w600,
+                                                color: Colors.red[600]))),
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text('${bomRoom.roomName}',
+                                            style: TextStyle(
+                                                fontSize: 15.0,
+                                                fontWeight: FontWeight.w600,
+                                                color: Colors.black)),
+                                        Text('10점 내기',
+                                            style: TextStyle(
+                                                fontSize: 12.0,
+                                                fontWeight: FontWeight.w600,
+                                                color: Colors.grey[600]))
+                                      ],
+                                    ),
+                                    Text('${bomRoom.participantsNum} / 8',
+                                        style: TextStyle(
+                                            fontSize: 12.0,
+                                            fontWeight: FontWeight.w600,
+                                            color: Colors.black))
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                ],
-              ),
-            ),
+                )
+                .toList(),
           ),
-        ),
-      )
-          .toList(),
-    ),
-  );
+        );
 }
