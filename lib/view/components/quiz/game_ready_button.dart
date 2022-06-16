225068f986976d897c8964e09913fa071c310978
shseok <gus0674@ajou.ac.kr>
2022-06-16 21:54:51 +0900

    Create new ready button

--- lib/view/components/quiz/game_ready_button.dart
+++ lib/view/components/quiz/game_ready_button.dart
@@ -1,6 +1,8 @@
 // isPartyLeader : true일 경우 시작 -> 단, 플레이어가 2명 이상일경우 겜임시작 버튼 활성화
 import 'package:bom_front/network/socket_method.dart';
 import 'package:bom_front/provider/room_data_provider.dart';
+import 'package:bom_front/utils/colors.dart';
+import 'package:bom_front/view/components/quiz/box_shadow.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -50,7 +52,7 @@ class _GameReadyButtonState extends ConsumerState<GameReadyButton> {
     int isRoomHost = ref.watch(roomDataProvider.notifier).state[3];
     return isRoomHost != 0
         ? (isBtn
-            ? CustomButton(
+            ? CustomGameButton(
                 onTap: () {
                   toggleButton();
                 },
@@ -67,4 +69,29 @@ class _GameReadyButtonState extends ConsumerState<GameReadyButton> {
           }
     });
   }
+
+  Widget CustomGameButton({required title, required onTap}){
+    return GestureDetector(
+        onTap: onTap,
+        child: Container(
+          margin: const EdgeInsets.all(10.0),
+          height: 50.0,
+          width: 100.0,
+          decoration: BoxDecoration(
+            color: bgColor,
+            boxShadow: boxShadow,
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            title,
+            style: const TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.w600,
+                color: Colors.white
+            ),
+          ),
+        ));
+  }
+
 }
